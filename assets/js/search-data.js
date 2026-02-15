ad9b6d577c7c6675f0a31f734bba87bc47c8a175
sebaeza <sebastianbaezag@gmail.com>
2026-02-15 22:57:07 +0000

    Deploying to gh-pages from @ sebaeza/sebastianbaeza@f24d7c42295e7942d805812da048788815957a30 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
           title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
@@ -43,11 +50,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+            },},{id: "projects-concentration-amp-co-localization",
+          title: 'Concentration &amp;amp; Co-localization',
+          description: "Spatial analysis of technological innovation patterns in Japan using patent data",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
+              window.location.href = "/projects/1_concentration_colocalization/";
+            },},{id: "projects-emerging-hot-spot-analysis",
+          title: 'Emerging Hot Spot Analysis',
+          description: "Tracing cluster life cycles in Japanese innovation using spatiotemporal statistics",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_ehsa_japan/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
