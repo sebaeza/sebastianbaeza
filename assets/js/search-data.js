3c41e92f7774001809cd551f206a872e32ad783c
sebaeza <sebastianbaezag@gmail.com>
2026-02-16 10:05:06 +0000

    Deploying to gh-pages from @ sebaeza/sebastianbaeza@616e62f6fbb79e0c5359c05a45dcd9cf4ec16834 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "publications",
-          description: "Published articles.",
+          description: "Peer-review published articles.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
