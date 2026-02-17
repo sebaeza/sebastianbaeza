895663ffa9ad9423279a49314a03a881d9eb5131
sebaeza <sebastianbaezag@gmail.com>
2026-02-17 00:19:41 +0000

    Deploying to gh-pages from @ sebaeza/sebastianbaeza@939c62d3989713ac58700e57ff33d9981ff19e54 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,12 +52,7 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "projects-concentration-amp-co-localization",
+      },{id: "projects-concentration-amp-co-localization",
           title: 'Concentration &amp;amp; Co-localization',
           description: "Spatial analysis of technological innovation patterns in Japan using patent data",
           section: "Projects",handler: () => {
